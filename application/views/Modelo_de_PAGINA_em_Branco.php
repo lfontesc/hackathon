0478342af4eb7bbfdef9d682f972aac84c8f0bcd
lucas <lfonteesc@gmail.com>
2018-11-18 16:40:45 -0300

    enviando

---
+++ application/views/Modelo_de_PAGINA_em_Branco.php
@@ -0,0 +1,18 @@
+
+    <!-- /.row -->
+    <!-- Main row -->
+    <div class="row">
+      <!-- Left col -->
+      <section class="col-lg-7 connectedSortable">
+        
+          Conteudo das Paginas Aqui
+      </section>
+      <!-- right col -->
+    </div>
+    <!-- /.row (main row) -->
+
+  </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  
